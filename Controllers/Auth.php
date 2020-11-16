b6201c5ac03070295ccf50449085ffbe1d548a1b
garodaj24 <garod700@gmail.com>
2020-11-16 12:44:06 +0400

    add else cases for issets uri

--- Controllers/Auth.php
+++ Controllers/Auth.php
@@ -3,6 +3,10 @@
     namespace Controllers;
 
     class Auth {
+        public function index() {
+            echo "inside Auth index";
+        }
+
         public function login() {
             echo "inside login";
         }
--- System/Routes.php
+++ System/Routes.php
@@ -5,28 +5,30 @@
     class Routes {
         public function __construct($uri) {
             try {
-                if (isset($uri[0]) && $uri[0] !== '') {
-                    if (file_exists($_SERVER['DOCUMENT_ROOT'] . "Controllers/$uri[0].php")) {
-                        if (class_exists("\\Controllers\\$uri[0]")) {
-                            $className = "\\Controllers\\$uri[0]";
-                            $route = new $className;
-                            if (isset($uri[1]) && $uri[1] !== '') {
-                                if (method_exists($route, $uri[1])) {
-                                    $route->{$uri[1]}();
-                                } else {
-                                    throw new \Exception("The method $uri[1] does not exist in the class $uri[0]");
-                                }
+                $ctrl = (isset($uri[0]) && $uri[0] !== '') ? ucfirst($uri[0]) : "Home";
+                if (file_exists($_SERVER['DOCUMENT_ROOT'] . "Controllers/$ctrl.php")) {
+                    if (class_exists("\\Controllers\\$ctrl")) {
+                        $className = "\\Controllers\\$ctrl";
+                        $ctrl_obj = new $className;
+                        if (isset($uri[1]) && $uri[1] !== '') {
+                            $method = $uri[1];
+                            if (method_exists($ctrl_obj, $method)) {
+                                $ctrl_obj->{$method}();
                             } else {
-                                #coming soon
+                                throw new \Exception("The method $uri[1] does not exist in the class $uri[0]");
                             }
                         } else {
-                            throw new \Exception("The class $uri[0] does not exist");
+                            if (method_exists($ctrl_obj, "index")) {
+                                $ctrl_obj->index();
+                            } else {
+                                throw new \Exception("The method index does not exist in the class $uri[0]");
+                            }
                         }
                     } else {
-                        throw new \Exception("The file Controllers/$uri[0].php does not exist");
+                        throw new \Exception("The class $uri[0] does not exist");
                     }
                 } else {
-                    #coming soon
+                    throw new \Exception("The file Controllers/$uri[0].php does not exist");
                 }
             } 
             catch(\Exception $e) {
