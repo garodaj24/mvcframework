fc2684acd232a6a855d69d8be8c72ddad4485230
garodaj24 <garod700@gmail.com>
2020-12-10 14:13:14 +0400

    include view file fix

--- System/View.php
+++ System/View.php
@@ -6,13 +6,13 @@
         public function render($filename, $layout=true) {
             try {
                 if (file_exists($_SERVER['DOCUMENT_ROOT'] . "Views/$filename.php")) {
-                    include $_SERVER['DOCUMENT_ROOT'] . "Views/$filename.php";
                     if ($layout) {
                         if (file_exists($_SERVER['DOCUMENT_ROOT'] . "Views/layout/Header.php") && file_exists($_SERVER['DOCUMENT_ROOT'] . "Views/layout/Footer.php")) {
+                            include $_SERVER['DOCUMENT_ROOT'] . "Views/$filename.php";
                             include $_SERVER['DOCUMENT_ROOT'] . "Views/layout/Header.php";
                             include $_SERVER['DOCUMENT_ROOT'] . "Views/layout/Footer.php";
                         } else {
-                            throw new \Exception("The files Views/layout/Header.php and Views/layout/Footer.php does not exist");
+                            include $_SERVER['DOCUMENT_ROOT'] . "Views/$filename.php";
                         }
                     }
                 } else {
